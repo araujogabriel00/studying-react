952234e18c491ae3fa3606dbf1aad2465c562233
araujogabriel00 <gabriel.araujos@sempreceub.com>
2022-09-03 15:39:24 -0300

    Subindo alterações

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,20 +1,30 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    const expenseDate = new Date(2022, 9, 3 );
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.7;
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.getDay()}</div>
+            <div>
+                <div>
+                    {month}
+                </div>
+                <div>
+                    {day}
+                </div>
+                <div>
+                    {year}
+                </div>
+            </div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{ }</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
-    );
+  );
 }
 
 export default ExpenseItem;
