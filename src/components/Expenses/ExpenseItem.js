37f137bb179ba0913927fbde88c850d24d2237fb
araujogabriel00 <gabriel.araujos@sempreceub.com>
2022-09-04 12:38:00 -0300

    A Closer Look at the "useState" Hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -5,12 +6,19 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Update!!!');
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
   );
