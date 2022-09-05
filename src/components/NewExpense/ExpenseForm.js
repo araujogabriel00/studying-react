cc4136250131114c3832bd0035c15ad73e0f51ec
araujogabriel00 <gabriel.araujos@sempreceub.com>
2022-09-05 15:44:25 -0300

    Format Ajust

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,63 +1,76 @@
 import React, { useState } from "react";
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmout, setEnteredAmout] = useState("");
+  const [enteredDate, setenteredDate] = useState("");
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmout, setEnteredAmout] = useState('');
-    const [enteredDate, setenteredDate] = useState('');
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
 
-    const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
+  const amountChangeHandler = (event) => {
+    setEnteredAmout(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setenteredDate(event.target.value);
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmout,
+      date: new Date(enteredDate),
     };
 
-    const amountChangeHandler = (event) => {
-        setEnteredAmout(event.target.value);
-    };
+    props.onSaveExpenseData(expenseData);
 
-    const dateChangeHandler = (event) => {
-        setenteredDate(event.target.value);
-    };
+    setEnteredTitle("");
+    setEnteredAmout("");
+    setenteredDate("");
+  };
 
-    const submitFormHandler = (event) => {
-        event.preventDefault();
-
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmout,
-            date: new Date(enteredDate),
-        };
-
-        props.onSaveExpenseData(expenseData);
-
-        setEnteredTitle('');
-        setEnteredAmout('');
-        setenteredDate('');
-
-
-    };
-
-    return (
-        <form onSubmit={submitFormHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}></input>
-                </div>
-                <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' value={enteredAmout} onChange={amountChangeHandler}></input>
-                </div>
-                <div className="new-expense__control">
-                    <label>Date</label>
-                    <input type='date' min='2022-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler}></input>
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
-            </div>
-        </form>
-    );
-}
+  return (
+    <form onSubmit={submitFormHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          ></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmout}
+            onChange={amountChangeHandler}
+          ></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          ></input>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
 
 export default ExpenseForm;
